c4eabb43dd3aa7113b30d0621a20cdcdfee58e58
Akshay Jagtap <57796960+AkshayJagtap9@users.noreply.github.com>
2024-08-17 20:44:06 +0530

    Add files via upload

---
+++ closure.js
@@ -0,0 +1,87 @@
+// Closure means a function binds together with its lexicle envoirnment
+
+/* A closure is the combination of the function bundle together (enclosed)
+ with reference to its surrounding state
+*/
+
+// v1
+/*
+function x(){
+    var a = 7;
+    function y(){
+        console.log(a);
+    }
+    y();
+}
+
+x();
+
+// o/p : 7
+
+*/
+
+// v2
+/*
+function x(){
+    var a = 7;
+    function y(){
+        console.log(a);
+    }
+    return y;
+}
+
+var z = x();
+console.log(z);
+
+z();
+*/
+
+/*
+o/p :
+function y(){
+    console.log(a);
+}
+7
+*/
+
+// v3
+/*
+function x(){
+    var a = 7;
+    function y(){
+        console.log(a);
+    }
+    a = 100;
+    return y;
+}
+var z = x();
+console.log(z);
+z();
+*/
+
+/*
+o/p :
+function y(){
+    console.log(a);
+}
+100
+*/
+
+
+// v4
+
+function z(){
+    var b = 900;
+    function x(){
+        var a = 7;
+        function y(){
+            console.log(a,b);
+        }
+        y();        
+    }
+    x();
+}
+z();
+
+//o/p: 7  900
+
